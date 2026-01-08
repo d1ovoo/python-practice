9023ad9cdec35d98e61a8f9ab003dcefcdf4e624
Diogo Pereira <dmmp08@gmail.com>
2026-01-08 17:45:51 +0000

    Add files via upload

---
+++ api-project.py
@@ -0,0 +1,23 @@
+import requests
+
+from urllib.parse import quote
+
+#THIS API DOESNT EXIST ANYMORE
+#IGNORE THIS CODE
+#url = "https://vlresports.vercel.app/v1/players"
+#region = input("What region?:")
+#map = input("What map?:")
+#agent = input("What agent?:")
+#country = input("What country?:")
+#params = {"region": region, "timespan": 90, "map": map, "agent": agent, "minrating": "1800", "country": country}
+#response = requests.get(url, params=params)
+#print(response.json())
+
+
+
+
+
+
+
+
+
